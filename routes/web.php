acc047f322dd3d8bd2ebdea80b6545aaf60e1a6d
AhmadShoffian <ahmadshof45@gmail.com>
2025-09-15 15:51:20 +0700

    update all

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontendController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [FrontendController::class, 'index']);
+Route::get('/login', [FrontendController::class, 'login'])->name('login');
+Route::get('/register', [FrontendController::class, 'register'])->name('register');
+
